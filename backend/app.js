a1bae8929af0361d493c0fde3a1c3ff82ca3bb23
Zakaria Abdullahi <zakaria-abdullahi@Zakarias-MacBook-Pro.local>
2023-02-09 15:34:22 -0800

    Added error-handling middleware

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+// backend/app.js
 const express = require('express');
 require('express-async-errors');
 const morgan = require('morgan');
@@ -37,7 +38,41 @@ app.use(
 );
 
 const routes = require('./routes');
-
 app.use(routes);
+
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+
+const { ValidationError } = require('sequelize');
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
+  
+
 
 module.exports = app;
