d1d71034555570aa3a470507f9d3d8f15c9b0d3b
Zakaria Abdullahi <zakaria-abdullahi@Zakarias-MacBook-Pro.local>
2023-02-11 12:29:00 -0800

    Added firstName and lastName attributes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,9 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
-const { restoreUser } = require('../../utils/auth.js');
+
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-
+const { restoreUser } = require('../../utils/auth.js');
 router.use(restoreUser);
 
 router.use('/session', sessionRouter);
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,44 +1,81 @@
-// backend/routes/api/users.js
+// // backend/routes/api/users.js
+// const express = require('express')
+// const { check } = require('express-validator');
+// const { handleValidationErrors } = require('../../utils/validation');
+// const { setTokenCookie, requireAuth } = require('../../utils/auth');
+// const { User } = require('../../db/models');
+
+// const router = express.Router();
+
+// const validateSignup = [
+//     // check('firstName')
+//     //   .exists({checkFalsy: true})
+//     //   .isLength({max: 30}),
+//     // check('lastName')
+//     //   .exists({checkFalsy: true})
+//     //   .isLength({max: 30}),
+//     check('email')
+//       .exists({ checkFalsy: true })
+//       .isEmail()
+//       .withMessage('Please provide a valid email.'),
+//     check('username')
+//       .exists({ checkFalsy: true })
+//       .isLength({ min: 4 })
+//       .withMessage('Please provide a username with at least 4 characters.'),
+//     check('username')
+//       .not()
+//       .isEmail()
+//       .withMessage('Username cannot be an email.'),
+//     check('password')
+//       .exists({ checkFalsy: true })
+//       .isLength({ min: 6 })
+//       .withMessage('Password must be 6 characters or more.'),
+//     handleValidationErrors
+//   ];
+
 const express = require('express')
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('../../utils/validation');
+
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
 const validateSignup = [
-    check('email')
-      .exists({ checkFalsy: true })
-      .isEmail()
-      .withMessage('Please provide a valid email.'),
-    check('username')
-      .exists({ checkFalsy: true })
-      .isLength({ min: 4 })
-      .withMessage('Please provide a username with at least 4 characters.'),
-    check('username')
-      .not()
-      .isEmail()
-      .withMessage('Username cannot be an email.'),
-    check('password')
-      .exists({ checkFalsy: true })
-      .isLength({ min: 6 })
-      .withMessage('Password must be 6 characters or more.'),
-    handleValidationErrors
-  ];
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
 
 // Sign up
 router.post(
     '/',
     validateSignup,
     async (req, res) => {
-      const { email, password, username } = req.body;
-      const user = await User.signup({ email, username, password });
+      const { firstName, lastName, email, password, username } = req.body;
+      const user = await User.signup({ firstName, lastName, email, username, password });
   
       await setTokenCookie(res, user);
   
       return res.json({
-        user
+        user,
       });
     }
   );
