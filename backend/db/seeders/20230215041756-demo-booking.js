0786221174421e9703c73cce0b948a3cc3e4ce6a
Zakaria Abdullahi <zakaria-abdullahi@Zakarias-MacBook-Pro.local>
2023-02-14 20:29:40 -0800

    Added Bookings table model,migration,and seeders

---
+++ backend/db/seeders/20230215041756-demo-booking.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    options.tableName = 'Bookings';
+    return queryInterface.bulkInsert(options, [
+      {
+        spotId: 1,
+        userId: 1,
+        startDate: '2023-10-22',
+        endDate: '2023-10-30'
+      },
+      {
+        spotId: 2,
+        userId: 2,
+        startDate: '2023-03-18',
+        endDate: '2023-03-28'
+      },
+      {
+        spotId: 3,
+        userId: 3,
+        startDate: '2023-12-25',
+        endDate: '2024-01-05'
+      }
+    ], {});
+    
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'Bookings';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      userId: {[Op.in]: [1, 2, 3]}
+    }, {})
+  }
+};
